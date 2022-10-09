4a9ed8350f2e934a88017ba1e9514fe25c442792
Arthur Werner Lira <105883063+wernerliraarthur@users.noreply.github.com>
2022-10-09 18:31:02 -0300

    contraste de cores

--- javaScript/myScript.js
+++ javaScript/myScript.js
@@ -1,3 +1,31 @@
+function normal() {
+    var root = document.querySelector(":root");
+    var root = document.querySelector(":root");
+    root.style.setProperty('--cor-do-fundo-red', 'red');
+    root.style.setProperty('--cor-do-fundo-cinza', '#c5c5c5');
+    root.style.setProperty('--cor-do-fundo-branco', 'white');
+    root.style.setProperty('--cor-do-texto-preto', 'black');
+    root.style.setProperty('--cor-do-texto-branco', 'white');
+}
+
+function preto() {
+    var root = document.querySelector(":root");
+    root.style.setProperty('--cor-do-fundo-red', 'black');
+    root.style.setProperty('--cor-do-fundo-cinza', 'black');
+    root.style.setProperty('--cor-do-fundo-branco', 'black');
+    root.style.setProperty('--cor-do-texto-preto', 'white');
+    root.style.setProperty('--cor-do-texto-branco', 'white');
+}
+
+function branco() {
+    var root = document.querySelector(":root");
+    root.style.setProperty('--cor-do-fundo-red', 'white');
+    root.style.setProperty('--cor-do-fundo-cinza', 'white');
+    root.style.setProperty('--cor-do-fundo-branco', 'white');
+    root.style.setProperty('--cor-do-texto-preto', 'black');
+    root.style.setProperty('--cor-do-texto-branco', 'black');
+}
+
 function azul() {
     var root = document.querySelector(":root");
     root.style.setProperty('--cor-do-fundo-red', '#000066');
